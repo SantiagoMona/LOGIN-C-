bafc8424ce7a4230d2a52d738ceb5930d693a0cd
SantiagoMona <143049137+SantiagoMona@users.noreply.github.com>
2024-04-15 18:55:56 -0500

    rer
    rere

---
+++ login/Controllers/AccesoController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace login.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login(){
+            return View();
+        }
+    }
+}
